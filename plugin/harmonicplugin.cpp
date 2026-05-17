44bf7c34810fbe750c8bcc09784128b52d5be985
Alex M. Lowe <alex.lowe@canonical.com>
2026-05-17 23:55:02 +0200

    Initialize KAboutData for plugin metadata (Issue #44)

--- plugin/harmonicplugin.cpp
+++ plugin/harmonicplugin.cpp
@@ -1,6 +1,7 @@
 #include "harmonicplugin.h"
 #include "harmonicconfigpage.h"
 
+#include <KAboutData>
 #include <KPluginFactory>
 #include <KLocalizedString>
 
@@ -11,6 +12,17 @@ HarmonicPlugin::HarmonicPlugin(QObject *parent, const QVariantList &args)
     : KTextEditor::Plugin(parent)
 {
     Q_UNUSED(args);
+
+    KAboutData aboutData(QStringLiteral("harmonicplugin"),
+                         i18n("Harmonic"),
+                         QStringLiteral("0.1.0"),
+                         i18n("AI-assisted vibecoding for Kate"),
+                         KAboutLicense::Custom,
+                         i18n("Copyright (c) 2024 Harmonic Contributors"));
+    aboutData.addAuthor(i18n("Harmonic Contributors"));
+    aboutData.setProductName("kate/harmonic");
+    // Note: KAboutData::setApplicationData(aboutData) is usually for apps,
+    // but initializing it here helps some KDE components find the metadata.
 }
 
 HarmonicPlugin::~HarmonicPlugin() = default;
