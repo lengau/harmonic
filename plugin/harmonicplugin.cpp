c82a5aea97b410ad81cf79040a7ebc2b5b333b90
Alex M. Lowe <alex.lowe@canonical.com>
2026-05-18 00:04:40 +0200

    Remove redundant KAboutData initialization

--- plugin/harmonicplugin.cpp
+++ plugin/harmonicplugin.cpp
@@ -1,9 +1,7 @@
 #include "harmonicplugin.h"
 #include "harmonicconfigpage.h"
 
-#include <KAboutData>
 #include <KPluginFactory>
-#include <KLocalizedString>
 
 K_PLUGIN_FACTORY_WITH_JSON(HarmonicPluginFactory, "harmonicplugin.json",
                            registerPlugin<HarmonicPlugin>();)
@@ -12,17 +10,6 @@ HarmonicPlugin::HarmonicPlugin(QObject *parent, const QVariantList &args)
     : KTextEditor::Plugin(parent)
 {
     Q_UNUSED(args);
-
-    KAboutData aboutData(QStringLiteral("harmonicplugin"),
-                         i18n("Harmonic"),
-                         QStringLiteral("0.1.0"),
-                         i18n("AI-assisted vibecoding for Kate"),
-                         KAboutLicense::Custom,
-                         i18n("Copyright (c) 2024 Harmonic Contributors"));
-    aboutData.addAuthor(i18n("Harmonic Contributors"));
-    aboutData.setProductName("kate/harmonic");
-    // Note: KAboutData::setApplicationData(aboutData) is usually for apps,
-    // but initializing it here helps some KDE components find the metadata.
 }
 
 HarmonicPlugin::~HarmonicPlugin() = default;
