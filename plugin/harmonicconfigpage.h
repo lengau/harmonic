acc28f285cacd53c2f7c37a4aa4183967cfbe83d
Alex M. Lowe <alex.lowe@canonical.com>
2026-05-18 06:46:11 +0200

    Apply clang-format (LLVM style)

--- plugin/harmonicconfigpage.h
+++ plugin/harmonicconfigpage.h
@@ -7,11 +7,10 @@ class QComboBox;
 class QLineEdit;
 class QCheckBox;
 
-class HarmonicConfigPage : public KTextEditor::ConfigPage
-{
+class HarmonicConfigPage : public KTextEditor::ConfigPage {
     Q_OBJECT
 
-public:
+  public:
     explicit HarmonicConfigPage(QWidget *parent = nullptr);
     ~HarmonicConfigPage() override;
 
@@ -19,12 +18,12 @@ public:
     QString fullName() const override;
     QIcon icon() const override;
 
-public Q_SLOTS:
+  public Q_SLOTS:
     void apply() override;
     void reset() override;
     void defaults() override;
 
-private:
+  private:
     QComboBox *m_backendCombo;
     QLineEdit *m_commandEdit;
     QLineEdit *m_apiKeyEdit;
